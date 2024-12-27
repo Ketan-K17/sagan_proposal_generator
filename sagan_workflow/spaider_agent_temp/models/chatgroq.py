243d74239a03f80689e0e698de035ce142f0ee72
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2024-12-27 13:22:19 +0530

    added data folder

--- sagan_workflow/spaider_agent_temp/models/chatgroq.py
+++
@@ -1,8 +0,0 @@
-from langchain_groq import ChatGroq
-from langchain_openai import ChatOpenAI
-
-def BuildChatGroq(model: str, temperature: float):
-    return ChatGroq(model=model, temperature=temperature)
-
-def BuildChatOpenAI(model: str, temperature: float):
-    return ChatOpenAI(model=model, temperature=temperature)
